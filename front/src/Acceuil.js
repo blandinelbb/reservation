96734358041c42cd4ec48654aa984b5b80cbf606
blandinelbb <linblandine@gmail.com>
2022-06-03 15:42:05 +0200

    creating home page

--- front/src/Acceuil.js
+++ front/src/Acceuil.js
@@ -1,11 +1,92 @@
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
 
 function Acceuil() {
   return (
     <div id="acceuil">
       <p>Bienvenue sur notre site de réservation </p>
-      <Button variant="contained">
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display: "inline-block",
+          margin: "60px",
+          borderRadius: "7px",
+        }}
+      >
+        <h4 id="typeSport">Tennis</h4>
+        <img src="../public/tennis.jpg" />
+      </Box>
+      <Box
+        activeclassname="image"
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display: "inline-block",
+          margin: "60px",
+          borderRadius: "7px",
+        }}
+      >
+        <h4 id="typeSport">Tennis</h4>
+        <img src="../public/judo.jpg" />
+      </Box>
+      <Box
+        activeclassname="image"
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display: "inline-block",
+          margin: "60px",
+          borderRadius: "7px",
+        }}
+      ></Box>
+      <Box
+        activeclassname="image"
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display: "inline-block",
+          margin: "60px",
+          borderRadius: "7px",
+        }}
+      ></Box>
+      <Box
+        activeclassname="image"
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display: "inline-block",
+          margin: "60px",
+          borderRadius: "7px",
+        }}
+      ></Box>
+      <p>Réserver maintenant le sport de votre choix</p>
+      <Button variant="contained" id="reserver" sx={{ margin: "50px 570px" }}>
         <Link
           exact="true"
           to="/reservation"
